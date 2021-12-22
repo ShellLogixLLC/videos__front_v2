0338dabc8f5daf1bf8440e4e896a9e9c5a868bb1
Martirosyan Mrd <martirosyanmrd@gmail.com>
2021-12-23 00:08:44 +0400

    unfinished task

--- src/components/shared/BackButton/index.tsx
+++ src/components/shared/BackButton/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {useRouter} from 'next/router';
 
+import {Route} from '~/constants';
+
 import Button from '../Button';
 
 import {BackButtonProp} from './types';
@@ -8,8 +10,15 @@ import {BackButtonProp} from './types';
 const BackButton: React.FC<BackButtonProp> = ({text, LeftIcon, className}) => {
   const router = useRouter();
 
+  const backHandler = () => {
+    if (router.back() === undefined) {
+      return Route.Home;
+    }
+    router.back();
+  };
+
   return (
-    <Button className={className} onClick={() => router.back()}>
+    <Button className={className} onClick={backHandler}>
       <LeftIcon />
       {text}
     </Button>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 export * from './routes';
 export * from './staticFilterBase';
 
-export {default as staticBase} from './staticBase';
+export {default as filmsBase} from './filmsBase';
 export {default as downloadFile} from './downloadFile';
 export {default as filteredMass} from './filteredMass';
