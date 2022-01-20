2bae9b28544b0b6039f7f913de3d8e6ba87f3cd4
Martirosyan Mrd <martirosyanmrd@gmail.com>
2022-01-20 15:56:11 +0400

    SHELL-5(UI):added category navbar component

---
+++ src/components/shared/CategoryNav/index.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import classNames from 'classnames';
+
+import {categoryNavigation} from '~/utils/index';
+
+import Button from '../Button';
+
+import styles from './CategoryNav.module.scss';
+
+const CategoryNav: React.FC = () => {
+  const [activeCategory, setActiveCategory] = useState<number>(0);
+
+  const buttonClasses = (id: number) =>
+    classNames(styles.wrapper__buttons, {
+      [styles.wrapper__buttons__active]: activeCategory === id,
+    });
+
+  const chooseCategory = (idx: number) => {
+    setActiveCategory(idx);
+  };
+
+  const renderCategoryNavigation = categoryNavigation.map(
+    ({nameCategory, id}, idx) => (
+      <Button
+        key={id}
+        onClick={() => chooseCategory(idx)}
+        className={buttonClasses(id)}>
+        {nameCategory}
+      </Button>
+    ),
+  );
+
+  return <nav className={styles.wrapper}>{renderCategoryNavigation}</nav>;
+};
+
+export default CategoryNav;
