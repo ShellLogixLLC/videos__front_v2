b409d1b359fd8ef9883da5a1db12fac6fa10faf3
Mko1999 <mkrtich.okroyan@gmail.com>
2022-05-31 16:55:10 +0400

    fix back button styling

--- src/containers/MyFavorites/index.tsx
+++ src/containers/MyFavorites/index.tsx
@@ -9,93 +9,88 @@ import styles from './MyFavorites.module.scss';
 const MyFavorites: React.FC = () => {
   return (
     <div className={styles.favorites}>
-      <Link
-        className={styles.favorites__link}
-        activeClassName={styles.favorites__link_active}
-        to={Route.Home}>
-        <div className={styles.favorites__backRoute}>
-          <BackButton
-            text="Back"
-            LeftIcon={LeftArrow}
-            className={styles.favorites__backRoute__button}
-          />
-        </div>
-      </Link>
+      <div className={styles.favorites__backRoute}>
+        <BackButton
+          text="Back"
+          LeftIcon={LeftArrow}
+          className={styles.favorites__backRoute__button}
+        />
+      </div>
 
       <Typography tagName="h1" className={styles.favorites__title}>
         WishList
       </Typography>
       <div className={styles.favorites__content}>
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
-        <Video
-          videoSrc={VIDEO_SRC}
-          posterSrc={POSTER_SRC}
-          videoDuration={VIDEO_LENGTH}
-        />
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
+        {/*<Video*/}
+        {/*  videoSrc={VIDEO_SRC}*/}
+        {/*  posterSrc={POSTER_SRC}*/}
+        {/*  videoDuration={VIDEO_LENGTH}*/}
+        {/*/>*/}
       </div>
       <div className={styles.favorites__pagination}></div>
     </div>
