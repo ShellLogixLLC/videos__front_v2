886fa81daa3f7f742be0b76deab4d3a7639866e7
Varsine <varsulmozg@gmail.com>
2022-07-04 14:42:41 +0400

    #SHELL-106 feet(bug): Refresh-after-login-homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,33 +7,51 @@ import 'emoji-mart/css/emoji-mart.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 import '~/styles/index.scss';
+import {useEffect} from 'react';
+
 import store, {wrapper} from '~/store';
+import {getCookieFromBrowser} from '~/libraries';
+import {authActions, authSelect} from '~/store/auth';
+import {useAppDispatch, useAppSelector} from '~/hooks';
 import {
   HistoryProvider,
   ModalContextProvider,
   ToastContextProvider,
 } from '~/context';
 
-const ProdApp: React.FC<AppProps> = ({Component, pageProps}) => (
-  <Provider store={store}>
-    <ModalContextProvider>
-      <ToastContextProvider>
-        <HistoryProvider>
-          <NextNprogress
-            height={3}
-            color="#f32d36"
-            stopDelayMs={200}
-            startPosition={0.3}
-            showOnShallow={true}
-            options={{
-              showSpinner: false,
-            }}
-          />
-          <Component {...pageProps} />
-        </HistoryProvider>
-      </ToastContextProvider>
-    </ModalContextProvider>
-  </Provider>
-);
+const ProdApp: React.FC<AppProps> = ({Component, pageProps}) => {
+  const dispatch = useAppDispatch();
+  const token = getCookieFromBrowser('token');
+  const {userInfo} = useAppSelector(authSelect);
+
+  useEffect(() => {
+    if (!userInfo && token) {
+      dispatch(authActions.loginWithToken({token: token as string}));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <ModalContextProvider>
+        <ToastContextProvider>
+          <HistoryProvider>
+            <NextNprogress
+              height={3}
+              color="#f32d36"
+              stopDelayMs={200}
+              startPosition={0.3}
+              showOnShallow={true}
+              options={{
+                showSpinner: false,
+              }}
+            />
+            <Component {...pageProps} />
+          </HistoryProvider>
+        </ToastContextProvider>
+      </ModalContextProvider>
+    </Provider>
+  );
+};
 
 export default wrapper.withRedux(appWithTranslation(ProdApp));
