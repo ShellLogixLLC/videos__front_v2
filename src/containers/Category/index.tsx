2c6db02cd215164a055fd1a5eeacbdb6293fd547
Mko1999 <mkrtich.okroyan@gmail.com>
2022-07-06 15:38:00 +0400

    minor improvements related to the icon names

--- src/containers/Category/index.tsx
+++ src/containers/Category/index.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {useRouter} from 'next/router';
 
-import {LeftArrow} from '~/assets';
+import {LeftArrowIcon} from '~/assets';
 import {QueryParamsTypes} from '~/types';
 import {filteredMass, setQueryParams} from '~/utils';
 import {DatePicker, Pagination, FilterBySort, BackButton} from '~/components';
@@ -42,7 +42,7 @@ const Category: React.FC = () => {
           <div className={styles.content__backRoute}>
             <BackButton
               text="Back"
-              LeftIcon={LeftArrow}
+              LeftIcon={LeftArrowIcon}
               className={styles.content__backRoute__button}
             />
           </div>
--- src/containers/WishList/index.tsx
+++ src/containers/WishList/index.tsx
@@ -8,7 +8,7 @@ import {
   Pagination,
   Typography,
 } from '~/components';
-import {LeftArrow} from '~/assets';
+import {LeftArrowIcon} from '~/assets';
 import WishlistSearchService from '~/api/wishlist';
 import {
   INITIAL_PAGINATION_ACTIVE_PAGE,
@@ -101,7 +101,7 @@ const MyFavorites: React.FC = () => {
       <div className={styles.favorites__backRoute}>
         <BackButton
           text="Back"
-          LeftIcon={LeftArrow}
+          LeftIcon={LeftArrowIcon}
           className={styles.favorites__backRoute__button}
         />
       </div>
