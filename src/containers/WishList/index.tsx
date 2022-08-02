a494d11c780b7e8942b66a85b2c78859ae55210c
Mko1999 <mkrtich.okroyan@gmail.com>
2022-08-02 15:02:27 +0400

    small changes

--- src/containers/WishList/index.tsx
+++ src/containers/WishList/index.tsx
@@ -58,9 +58,6 @@ const MyFavorites: React.FC = () => {
 
   const {translatedTypo} = useLocales('back');
 
-  console.log(data, 'data');
-  console.log(totalCount, 'count');
-
   useEffect(() => {
     if (!isLoading) {
       setVideosList(data.videos);
