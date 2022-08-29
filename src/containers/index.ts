85d42b93d2e52b31b624d86d8c16b24e14d03de0
Mko1999 <mkrtich.okroyan@gmail.com>
2022-08-29 19:01:51 +0400

    test

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,8 +1,3 @@
-// Registration
-export {default as RegistrationSetupPasswordContainer} from './registration/RegistrationSetupPassword';
-export {default as RegistrationContactInformationContainer} from './registration/RegistrationContactInformation';
-export {default as RegistrationPersonalInformationContainer} from './registration/RegistrationPersonalInformation';
-
 export {default as Home} from './Home';
 export {default as Error} from './Error';
 export {default as Search} from './Search';
