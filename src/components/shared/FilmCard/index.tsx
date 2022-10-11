7314518bbd324819eac853df54c53e216b38d8e7
Mkrtich Okroyan <mkrtich.okroyan@gmail.com>
2022-10-11 19:34:15 +0400

    add wishlist count

--- src/components/shared/FilmCard/index.tsx
+++ src/components/shared/FilmCard/index.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import classNames from 'classnames';
 
 import {VideoLikes} from '~/components';
-import {useAppDispatch} from '~/hooks';
+import {useAppDispatch, useAppSelector} from '~/hooks';
 import {UnRegisterPopup} from '~/components';
 import {getCookieFromBrowser} from '~/libraries';
 import {createDate, sleep, WarnToast} from '~/utils';
