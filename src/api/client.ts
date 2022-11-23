a9f70b614df044bab92c0bfd0add93d0997ab38e
Mkrtich Okroyan <mkrtich.okroyan@gmail.com>
2022-11-23 13:55:32 +0400

    add video view endpoint

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,7 +3,7 @@ import axios, {AxiosRequestConfig} from 'axios';
 import {getCookieFromBrowser} from '~/libraries';
 
 export const defaultOptions = {
-  baseURL: 'https://afternoon-plains-03331.herokuapp.com/api',
+  baseURL: 'https://obscure-harbor-76716.herokuapp.com/api',
 };
 
 const api = axios.create(defaultOptions);
